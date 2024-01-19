26773b0e05826702d9a99e16f2b548b5e9d8741e
Damicable <annobidiegwu@gmail.com>
2024-01-19 03:47:41 +0000

    Meeting view

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -1,2 +1,6 @@
--- Creating view SQL statement
-CREATE VIEW need_meeting AS SELECT name FROM students WHERE students.score < 80 OR DATE_SUB(CURRENT_DATE(), INTERVAL 1 MONTH);
+-- Create the 'need_meeting' view
+CREATE VIEW need_meeting AS
+    SELECT student_name
+    FROM students
+    WHERE score < 80
+      AND (last_meeting IS NULL OR last_meeting < NOW() - INTERVAL 1 MONTH);
