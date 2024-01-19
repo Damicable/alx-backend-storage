38fb45b318a902be9ef7d1701878ba6ba228ceda
Damicable <annobidiegwu@gmail.com>
2024-01-19 03:37:15 +0000

    Valid email

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,8 +1,4 @@
--- Task: Create a table 'users' with specified attributes
-
--- Check if the table already exists
-IF NOT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = 'users') THEN
-	-- User table creation
+-- Create a table 'users' with specified attribes
 CREATE TABLE IF NOT EXISTS users(
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	email VARCHAR(255) NOT NULL UNIQUE,
