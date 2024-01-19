4b1cdfa25c0c2db3c74230296020b41d19ec3b4b
Damicable <annobidiegwu@gmail.com>
2024-01-19 03:42:11 +0000

    Safe divide

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -1,12 +1,11 @@
--- Creating a function
+-- Create a function SafeDiv
 DELIMITER &&
 CREATE FUNCTION SafeDiv(a INT, b INT) RETURNS FLOAT
 BEGIN
-	DECLEAR result FLOAT DEFAULT 0;
+	DECLARE result FLOAT DEFAULT 0;
 	IF b != 0 THEN
 		SET result = a / b;
 	END IF;
 	RETURN result;
-
 END &&
 DELIMITER ;
