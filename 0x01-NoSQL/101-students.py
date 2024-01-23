2dba6fe6eacf75f6826be32aa64692f6e2f2cea5
Damicable <annobidiegwu@gmail.com>
2024-01-23 22:22:21 +0000

    Nginx log stats

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -8,4 +8,15 @@ def top_students(mongo_collection):
     @mongo_collection: pymongo collection object.
     Return: Average score
     """
+    if mongo_collection.count_documents({}) == 0:
+        return []
 
+    return mongo_collection.aggregate([
+            {
+                '$project': {
+                    'name': '$name',
+                    'averageScore': {'$avg': '$topics.score'}
+                }
+            },
+            {'$sort': {'averageScore': -1}}
+        ])
--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,37 +1,22 @@
-#!/usr/bin/env python3
-"""Nginx log stats"""
+"""Nginx log stats module """
 
 
 from pymongo import MongoClient
 
 if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
-    db_count = client.logs.nginx.count_documents({})
+    db_count = client.logs.nginx.count({})
     methods_dict = {
-                'GET': client.logs.nginx.count_documents({
-                        'method': 'GET'
-                    }),
-                'POST': client.logs.nginx.count_documents({
-                        'method': 'POST'
-                    }),
-                'PUT': client.logs.nginx.count_documents({
-                        'method': 'PUT'
-                    }),
-                'PATCH': client.logs.nginx.count_documents({
-                        'method': 'PATCH'
-                    }),
-                'DELETE': client.logs.nginx.count_documents({
-                        'method': 'DELETE'
-                    }),
+                'GET': client.logs.nginx.count({'method': 'GET'}),
+                'POST': client.logs.nginx.count({'method': 'POSTT'}),
+                'PUT': client.logs.nginx.count({'method': 'PUT'}),
+                'PATCH': client.logs.nginx.count({'method': 'PATCH'}),
+                'DELETE': client.logs.nginx.count({'method': 'DELETE'}),
             }
-    get_status = client.logs.nginx.count_documents({
-            'method': 'GET',
-            'path': '/status'
-        })
+    get_status = client.logs.nginx.count({'method': 'GET', 'path': '/status'})
 
     print("{} logs".format(db_count))
     print("Methods: ")
     for key, val in methods_dict.items():
         print("\tmethod {}: {}".format(key, val))
     print("{} status check".format(get_status))
-    client.close()
