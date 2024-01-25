8271d7a94847f1649b99cfd9393a6353cbfc939d
Damicable <annobidiegwu@gmail.com>
2024-01-25 05:04:47 +0000

    New log stata

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -1,46 +1,42 @@
 #!/usr/bin/env python3
-"""Nginx stats"""
+"""New Nginx log stats"""
 
 
 from pymongo import MongoClient
 
-if __name__ == "__main__":
+
+def log_stats():
+    """ log_stats.
+    """
     client = MongoClient('mongodb://127.0.0.1:27017')
-
-    # Count total logs
-    db_count = client.logs.nginx.count_documents({})
-
-    # Count methods
-    methods_dict = {
-        'GET': client.logs.nginx.count_documents({'method': 'GET'}),
-        'POST': client.logs.nginx.count_documents({'method': 'POST'}),
-        'PUT': client.logs.nginx.count_documents({'method': 'PUT'}),
-        'PATCH': client.logs.nginx.count_documents({'method': 'PATCH'}),
-        'DELETE': client.logs.nginx.count_documents({'method': 'DELETE'}),
-    }
-
-    # Count status check
-    get_status = client.logs.nginx.count_documents({'method': 'GET', 'path': '/status'})
-
-    # Count top 10 IPs
-    top_ips = client.logs.nginx.aggregate([
-        {"$group": {"_id": "$ip", "count": {"$sum": 1}}},
-        {"$sort": {"count": -1}},
-        {"$limit": 10}
-    ])
-
-    print("{} logs".format(db_count))
-    
-    # Print methods count
-    print("Methods: ")
-    for key, val in methods_dict.items():
-        print("\tmethod {}: {}".format(key, val))
-    
-    print("{} status check".format(get_status))
-
-    # Print top 10 IPs
+    logs_collection = client.logs.nginx
+    total = logs_collection.count_documents({})
+    get = logs_collection.count_documents({"method": "GET"})
+    post = logs_collection.count_documents({"method": "POST"})
+    put = logs_collection.count_documents({"method": "PUT"})
+    patch = logs_collection.count_documents({"method": "PATCH"})
+    delete = logs_collection.count_documents({"method": "DELETE"})
+    path = logs_collection.count_documents(
+        {"method": "GET", "path": "/status"})
+    print(f"{total} logs")
+    print("Methods:")
+    print(f"\tmethod GET: {get}")
+    print(f"\tmethod POST: {post}")
+    print(f"\tmethod PUT: {put}")
+    print(f"\tmethod PATCH: {patch}")
+    print(f"\tmethod DELETE: {delete}")
+    print(f"{path} status check")
     print("IPs:")
-    for ip in top_ips:
-        print("\t{}: {}".format(ip["_id"], ip["count"]))
+    sorted_ips = logs_collection.aggregate(
+        [{"$group": {"_id": "$ip", "count": {"$sum": 1}}},
+         {"$sort": {"count": -1}}])
+    i = 0
+    for s in sorted_ips:
+        if i == 10:
+            break
+        print(f"\t{s.get('_id')}: {s.get('count')}")
+        i += 1
 
-    client.close()
+
+if __name__ == "__main__":
+    log_stats()
