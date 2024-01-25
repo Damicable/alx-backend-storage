7d9f5909c1ca9f766f6a24bb7e6e878a263c3506
Damicable <annobidiegwu@gmail.com>
2024-01-25 06:21:49 +0000

    Added

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,43 +1,18 @@
 #!/usr/bin/env python3
-""" Web cache implementation"""
+"""Web cache impementation"""
 
 
 import requests
 import redis
-from functools import wraps
-
-store = redis.Redis()
 
 
-def count_url_access(method):
-    """
-    count_url_access - A decorator counting how many times
-    a Url is accessed
-    @method: Method to count 
-    """
-    @wraps(method)
-    def wrapper(url):
-        cached_key = "cached:" + url
-        cached_data = store.get(cached_key)
-        if cached_data:
-            return cached_data.decode("utf-8")
-
-        count_key = "count:" + url
-        html = method(url)
-
-        store.incr(count_key)
-        store.set(cached_key, html)
-        store.expire(cached_key, 10)
-        return html
-    return wrapper
-
-
-@count_url_access
 def get_page(url: str) -> str:
     """
     get_page - Fetch pages from a url
     @url: Url to fetch from
     Returns: String
     """
-    res = requests.get(url)
-    return res.text
+    redis_instance = redis.Redis()
+    contents = requests.get(url).text
+    r.setex(url, 10, contents)
+    r.incr('count' + ':' + url)
